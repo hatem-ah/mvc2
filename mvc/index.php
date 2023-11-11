c8a657bb2c7915790eeaa35d61e6f751e2af5c8a
hatem-ah <jameswillis19911991@gmail.com>
2023-11-11 07:39:40 -0500

    edit

--- mvc/index.php
+++ mvc/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ .'/app/models/UserModel.php';
 require_once __DIR__ .'/config/config.php';
 require_once __DIR__ . '/lib/DB/MysqliDb.php';
 
-
+//.....
 
 
 $config = require 'config/config.php';
